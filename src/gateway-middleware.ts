4ed911e8f5d6b5677cf1fef8e0e70ca2f5074846
CauaHenrique19 <cauah6002@gmail.com>
2024-01-09 19:15:35 -0300

    added helpers and github action

---
+++ src/gateway-middleware.ts
@@ -0,0 +1,28 @@
+import JWT from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import { NotAuthorizedError } from "./error-handler";
+
+const tokens: string[] = ["auth", "seller", "gig", "search", "buyer", "message", "order", "review"];
+
+export async function verifyGatewayRequest(req: Request, res: Response, next: NextFunction): Promise<void> {
+  if (!req.headers.gatewayToken) {
+    throw new NotAuthorizedError(
+      "Invalid Request",
+      "verifyGatewayRequest() method Request not coming from api gateway",
+    );
+  }
+
+  const token: string = req.headers.gatewayToken as string;
+
+  try {
+    const payload = JWT.verify(token, "") as { id: string; iat: number };
+    if (!tokens.includes(payload.id)) {
+      throw new NotAuthorizedError("Invalid Request", "verifyGatewayRequest() method Request payload is invalid");
+    }
+  } catch (error) {
+    throw new NotAuthorizedError(
+      "Invalid Request",
+      "verifyGatewayRequest() method Request not coming from api gateway",
+    );
+  }
+}
